8cb09a68c3f1b2d60172e0b7c7a5387d1b67b857
jacuSwag <jacek.majdanik@gmail.com>
2025-05-15 12:07:08 +0200

    Dodano strukture

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>Witamy w Password Managerze 🔐</h1>
+    <p>To działa!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+}
+</style>
